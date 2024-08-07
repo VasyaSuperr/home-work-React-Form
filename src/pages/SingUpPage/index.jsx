795ca02f248530c2c64c1956f4c918c4a145f6b6
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-08-07 23:38:05 +0300

    Add main style

--- src/pages/SingUpPage/index.jsx
+++ src/pages/SingUpPage/index.jsx
@@ -2,57 +2,67 @@ import styles from "./SingUp.module.css";
 
 function SingUpPage() {
   return (
-    <div>
-      <i class="fa-regular fa-pen-to-square"></i>
-      <h1>Create Your Account</h1>
-      <form>
-        <label>
-          <span>FULL NAME</span>
-          <input
-            type="text"
-            name="user-fullName"
-            placeholder="John Doe"
-            autoFocus
-            required
-          />
-        </label>
-        <label>
-          <span>EMAIL ADDRESS</span>
-          <input
-            type="email"
-            name="user-email"
-            placeholder="johndoe@gmail.com"
-            required
-          />
-        </label>
-        <label>
-          <span>PASSWORD</span>
-          <input
-            type="password"
-            name="user-password"
-            placeholder="Password"
-            required
-          />
-        </label>
-        <label>
-          <span>PASSWORD CONFIRMATION</span>
-          <input
-            type="password"
-            name="user-password-confirmation"
-            placeholder="Password confirmation"
-            required
-          />
-        </label>
-        <label>
-          <input type="checkbox" name="user-agree-all-statements" required />
-          <span>I Agree All Statements In Terms Of Service</span>
-        </label>
-        <button type="submit">Sing Up</button>
-        <div>
-          <p>I'm already a member!</p>
-          <button>Sing In</button>
-        </div>
-      </form>
+    <div className={styles.wrapperWrapper}>
+      <div className={styles.formWrapper}>
+        <i className={`${styles.iconCreate} fa-regular fa-pen-to-square`}></i>
+        <h2 className={styles.formHead}>Create Your Account</h2>
+        <form className={styles.singUpForm}>
+          <label className={styles.formLabelForInput}>
+            <span className={styles.labelCaptionForInput}>FULL NAME</span>
+            <input
+              className={styles.formInput}
+              type="text"
+              name="user-fullName"
+              placeholder="John Doe"
+              autoFocus
+              required
+            />
+          </label>
+          <label className={styles.formLabelForInput}>
+            <span className={styles.labelCaptionForInput}>EMAIL ADDRESS</span>
+            <input
+              className={styles.formInput}
+              type="email"
+              name="user-email"
+              placeholder="johndoe@gmail.com"
+              required
+            />
+          </label>
+          <label className={styles.formLabelForInput}>
+            <span className={styles.labelCaptionForInput}>PASSWORD</span>
+            <input
+              className={styles.formInput}
+              type="password"
+              name="user-password"
+              placeholder="Password"
+              required
+            />
+          </label>
+          <label className={styles.formLabelForInput}>
+            <span className={styles.labelCaptionForInput}>
+              PASSWORD CONFIRMATION
+            </span>
+            <input
+              className={styles.formInput}
+              type="password"
+              name="user-password-confirmation"
+              placeholder="Password confirmation"
+              required
+            />
+          </label>
+          <label className={styles.formLabelForCheckBox}>
+            <input type="checkbox" name="user-agree-all-statements" required />
+            <span>I Agree All Statements In Terms Of Service</span>
+          </label>
+          <button className={styles.submitBtn} type="submit">
+            Sing Up
+          </button>
+          <div className={styles.wrapperForSingInRow}>
+            <span>I'm already a member!</span>
+            <button className={styles.singInBtn}>Sing In</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
